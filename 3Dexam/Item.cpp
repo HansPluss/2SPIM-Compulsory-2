fe8a56688886444f13250bd8634992b21e422ef3
Tord-Elias <tordeliashoumb46@gmail.com>
2024-10-15 23:21:23 +0200

    fixed a bug in Item.cpp
    Item had a issue where it could not use the collitionComponent (idk why), so i coded collition in Item

--- 3Dexam/Item.cpp
+++ 3Dexam/Item.cpp
@@ -1,30 +1,36 @@
 #include "Item.h"
 #include "Player.h"
-#include "CollisionSystem.h"
+#include "Component.h"
+#include <vector>
 
-
-Item::Item()
+Item::Item() : ItemID(0)
 {
-		AddComponent<PositionComponent>();
-		AddComponent<RenderComponent>(glm::vec3(0.0f), glm::vec3(1.0f), "cube");
-		AddComponent<CollisionComponent>();
+    AddComponent<PositionComponent>();
+    AddComponent<RenderComponent>(glm::vec3(0.0f), glm::vec3(1.0f), "cube");
+    // AddComponent<CollisionComponent>();
 }
 
 Item::~Item()
 {
 }
 
-
-void Item::Pickup(Player player)
+void Item::Pickup(Player& player)
 {
-	player.AddInventoryItem(ItemID);
+	//adds a random item to the player's inventory
+    player.AddInventoryItem(ItemID);
 }
 
-void Item::checkCollision(Player player)
+void Item::checkCollision(Player& player) 
 {
-	CollisionSystem collisionSystem;
-	if (collisionSystem.SphereCollision(player, *this, 0.0f))
-	{
-		Pickup(player);
-	}
+    glm::vec3 futurePosA = player.GetComponent<PositionComponent>()->GetPosition()
+        + player.GetComponent<VelocityComponent>()->GetVelocity();
+
+    // Calculate the distance between the centers of both objects
+    float distanceCenters = glm::length(futurePosA - this->GetComponent<PositionComponent>()->GetPosition());
+
+    // Check if the distance is less than the sum of the radius (collision detection)
+    if (distanceCenters <= (player.GetComponent<RenderComponent>()->size.x + this->GetComponent<RenderComponent>()->size.x))
+    {
+        Pickup(player); 
+    }
 }
--- 3Dexam/Item.h
+++ 3Dexam/Item.h
@@ -1,15 +1,17 @@
 #pragma once
 #include "Entity.h"
 #include "Component.h"
-class Player; 
+#include "Player.h"
+class CollisionSystem;
+class Player;
 class Item : public Entity {
 public:
 	Item();
 	~Item();
-	void checkCollision(Player player);
+	void checkCollision(Player& player);
 
 private:
-	void Pickup(Player player);
+	void Pickup(Player& player);
 	int ItemID = rand() % 3; // 0, 1, 2
 
 };
